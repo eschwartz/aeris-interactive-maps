4c605ec4e94fd4623e4b854b359350f3b89304d1
Edan Schwartz <edan@edanschwartz.com>
2014-04-04 17:19:28 -0500

    TEMP: comment out submodules besides layers
    - so I can focus on refactoring layers

--- src/maps/mapapp/config/submodules.js
+++ src/maps/mapapp/config/submodules.js
@@ -2,11 +2,11 @@ define({
   $exports: {
     map: { wire: 'aim/maps/map/config/module' },
     mapControls: { wire: 'aim/maps/mapcontrols/config/module' },
-    markers: { wire: 'aim/maps/markers/config/module' },
+    //markers: { wire: 'aim/maps/markers/config/module' },
     layers: { wire: 'aim/maps/layers/config/module' },
-    infoPanel: { wire: 'aim/maps/infopanel/config/module'},
+    /*infoPanel: { wire: 'aim/maps/infopanel/config/module'},
     geosearch: { wire: 'aim/maps/geosearch/config/module' },
     modal: { wire: 'aim/maps/modal/config/module' },
-    fullscreen: { wire: 'aim/maps/fullscreen/config/module' }
+    fullscreen: { wire: 'aim/maps/fullscreen/config/module' }*/
   }
 });
