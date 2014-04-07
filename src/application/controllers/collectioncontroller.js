ed77177ea2f24d222b4bc4913a0bc0c9558ab54c
Edan Schwartz <edan@edanschwartz.com>
2014-04-07 10:53:57 -0500

    CollectionController: itemControllerLookup option
    - Allows item controller types
      to be configured based on model id

--- src/application/controllers/collectioncontroller.js
+++ src/application/controllers/collectioncontroller.js
@@ -16,15 +16,40 @@ define([
    * @implements aeris.application.controllers.ControllerInterface
    *
    * @constructor
+   *
+   * @param {Object} options
+   * @param {aeris.Collection} options.collection
+   * @param {Object.<string,aeris.application.controllers.ControllerInterface>} options.itemControllerLookup A map of controllers to use,
+   *        based on the model id.
    */
   var CollectionController = function(options) {
     _.defaults(options, {
       itemView: ItemController
     });
 
+    /**
+     * @property itemControllerLookup_
+     * @private
+     * @type {Object}
+     */
+    this.itemControllerLookup_ = options.itemControllerLookup;
+
+
     CollectionView.call(this, options);
   };
   _.inherits(CollectionController, CollectionView);
+
+
+  /**
+   * Overrides {Marionette.CollectionView}#getItemView
+   *
+   * @method getItemView
+   * @protected
+   * @param {aeris.Model} model
+   */
+  CollectionController.prototype.getItemView = function(model) {
+    return this.itemControllerLookup_[model.id] || this.options.itemView;
+  };
 
 
   return CollectionController;
---
+++ tests/spec/application/controllers/collectioncontroller.js
@@ -0,0 +1,92 @@
+define([
+  'aeris/util',
+  'aim/application/controllers/collectioncontroller',
+  'aim/application/controllers/itemcontroller',
+  'tests/mocks/template',
+  'aeris/collection',
+  'aeris/model'
+], function(_, CollectionController, ItemController, mockTemplate, Collection, Model) {
+
+  var MockControllerTypeFactory = function(typeId) {
+    var MockControllerType = function(opt_options) {
+      var options = _.defaults(opt_options || {}, {
+        template: mockTemplate
+      });
+      ItemController.call(this, options);
+    };
+    _.inherits(MockControllerType, ItemController);
+
+    MockControllerType.prototype.jasmineToString = function() {
+      return typeId;
+    };
+
+    MockControllerType.jasmineToString = function() {
+      return 'Type_' + typeId + '_';
+    };
+
+    return MockControllerType;
+  };
+
+
+
+  describe('CollectionController', function() {
+    var collectionController;
+    var itemControllerLookup, DefaultController;
+    var collection, modelA, modelB, modelC;
+
+    beforeEach(function() {
+      itemControllerLookup = {
+        A: MockControllerTypeFactory('A'),
+        B: MockControllerTypeFactory('B'),
+        C: MockControllerTypeFactory('C')
+      };
+
+      modelA = new Model({ id: 'A' });
+      modelB = new Model({ id: 'B' });
+      modelC = new Model({ id: 'C' });
+
+      DefaultController = MockControllerTypeFactory('DEFAULT');
+
+      collection = new Collection();
+
+      collectionController = new CollectionController({
+        itemControllerLookup: itemControllerLookup,
+        collection: collection,
+        itemView: DefaultController
+      });
+    });
+
+
+    describe('render', function() {
+
+      it('should render controllers according to the itemControllerLookup', function() {
+        collection.add(modelA);
+        collection.add(modelB);
+
+        collectionController.render();
+
+        expect(collectionController.children.findByModel(modelA)).
+          toBeInstanceOf(itemControllerLookup.A);
+        expect(collectionController.children.findByModel(modelB)).
+          toBeInstanceOf(itemControllerLookup.B);
+      });
+
+      describe('when no corresponding controller is defined in the lookup', function() {
+
+        it('should render the default itemView option', function() {
+          var model = new Model({
+            id: 'no_corresponding_controller'
+          });
+          collection.add(model);
+
+          expect(collectionController.children.findByModel(model)).
+            toBeInstanceOf(DefaultController);
+        });
+
+      });
+
+    });
+
+  });
+
+});
