648a881b592509cd12dc8403f05e94261462d0f8
Edan Schwartz <eschwartz@weathernation.net>
2014-04-09 10:04:37 -0500

    Style guide: update widgets

--- src/themes/styleguide/default/js/widgets.js
+++ src/themes/styleguide/default/js/widgets.js
@@ -1,24 +1,19 @@
 require.config({
   baseUrl: '../../../../'
-})
+});
 require([
   'config-amd'
 ], function() {
   require([
     'aim/application/widgets/slider/slider'
   ], function(Slider) {
-    var $container = $('.aeris-maps-app');
-    var slider = new Slider({
-      label: 'Continuous'
-    });
-    var steppedSlider = new Slider({
-      step: 20,
-      label: 'Stepped'
-    });
-    slider.render();
-    slider.$el.appendTo($container);
+    $('.sg-slider').each(function() {
+      var steppedSlider = new Slider({
+        step: 20
+      });
 
-    steppedSlider.render();
-    steppedSlider.$el.appendTo($container);
+      steppedSlider.render();
+      steppedSlider.$el.appendTo($(this));
+    });
   });
 });
